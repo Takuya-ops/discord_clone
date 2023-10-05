7a9b5d48d4fdc17fd8238f22653dec47ed7a0617
Takuya-ops <hora2112.ipu@gmail.com>
2023-10-05 23:45:45 +0900

    scss add

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>discord</h1>
     </div>
   );
 }
