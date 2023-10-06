d89fefd0d6c8ef044a12e36f860f52ac91432d38
Takuya-ops <hora2112.ipu@gmail.com>
2023-10-06 23:26:06 +0900

    sidebarChannels add

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./Sidebar.scss"
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AddIcon from '@mui/icons-material/Add';
+import SidebarChannel from './SidebarChannel';
 
 const Sidebar = () => {
   return (
@@ -18,6 +20,31 @@ const Sidebar = () => {
           <h3>Discord</h3>
           <ExpandMoreIcon/>
         </div>
+        <div className='sidebarChanels'>
+          <div className='sidebarChanelsHeader'>
+          </div>
+            <div className='sidebarHeader'>
+              <ExpandMoreIcon/>
+              <h4>プログラミング</h4>
+              <AddIcon className='sidebarAddIcon'/>
+            </div>
+            <div className='sidebarChannelList'>
+              <SidebarChannel/>
+              <SidebarChannel/>
+              <SidebarChannel/>
+              <SidebarChannel/>
+              <SidebarChannel/>
+            </div>
+            <div className='sidebarFooter'>
+              <div className='sidebarAccount'>
+                <img src="./logo512.png" alt=''/>
+                <div className='accountName'>
+                  <h4>ShinCode</h4>
+                  <span>#8162</span>
+                </div>
+              </div>
+            </div>
+          </div>
       </div>
     </div>
   )
---
+++ src/components/sidebar/SidebarChannel.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./SidebarChannel.scss"
+
+const SidebarChannel = () => {
+  return (
+    <div>
+      <h4 className='sidebarChannel'>
+        <span className='sidebarChannelHash'>#</span>
+        youtube
+      </h4>
+    </div>
+  )
+}
+
+export default SidebarChannel
